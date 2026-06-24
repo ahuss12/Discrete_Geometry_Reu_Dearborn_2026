84e99bbfea5062c31c301126e330a43596199aff
felixksant <vimdih-9vybwa-huRhyg>
2026-06-24 10:19:17 -0400

    added policy and value heads

--- gnn_pyg_files/RGAT.py
+++ gnn_pyg_files/RGAT.py
@@ -34,8 +34,42 @@ class RGAT(nn.Module):
         for i, conv in enumerate(self.convs):
             residual = x
             x = conv(x, edge_index, edge_type)
-            if i < len(self.convs) -1: 
+              if i < len(self.convs) -1: 
                 x = self.norms[i](x)
                 x = F.gelu(x)
             x = x + residual if i != 0 and i != len(self.convs)-1 else x
         return x
+
+## gives an embedding of the entire graph based on the node-level embedding output from GNN
+def graphEmbed(embeddings: Dict[str, Tensor]) -> Tensor:
+    return sum(t.sum(dim = 0) for t in embeddings.values()) ## note: worse subdivisions will actually have a larger sum. take into consideration. 
+
+## value head takes in graph-level embedding vector of shape (in_channels,) and outputs a scalar. 
+class valueHead(nn.Module): 
+    def __init__(self, in_channels: int = padding_level, hidden_channels: int = 32):
+        super().__init__()
+        self.norm = nn.LayerNorm(in_channels)
+        self.fc1 = nn.Linear(in_channels, hidden_channels)
+        self.fc2 = nn.Linear(hidden_channels, 1)
+
+    def forward(self, x):
+        x = self.norm(x)
+        x = F.gelu(self.fc1(x))
+        x = self.fc2(x)
+        return x.squeeze(-1)
+
+## outputs a probability distribution over the possible actions
+class policyHead(nn.Module): 
+    def __init__(self, in_channels: int = 2 * padding_level, hidden_channels: int = 128):
+        super().__init__()
+        self.norm = nn.LayerNorm(in_channels)
+        self.fc1 = nn.Linear(in_channels, hidden_channels)
+        self.fc2 = nn.Linear(hidden_channels, 1)
+
+    ## Z are the embeddings of the lattice-action nodes in the graph. g is the graph embedding. 
+    def forward(self, Z, g):
+        x = torch.cat([Z, g.unsqueeze(0).expand(Z.size(0), -1)], dim = -1)
+        x = self.norm(x)
+        x = F.gelu(self.fc1(x))
+        logits = self.fc2(x).squeeze(-1)
+        return F.log_softmax(logits, dim = 0)
